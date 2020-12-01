f7ea1d92b161df907ed828064141836ba0dace9e
HatPinDotCom <73682089+HatPinDotCom@users.noreply.github.com>
2020-12-01 13:44:52 +0530

    Timer code

--- timer.py
+++ timer.py
@@ -1,1 +1,10 @@
-print("hello")
+import time
+setTime = int(input("Set your timer in seconds:"))
+n = 1
+ 
+while n:
+    setTime = setTime - 1
+    print(setTime)
+    time.sleep(1)
+    if setTime <= 0:
+        print('Time\'s up.')
